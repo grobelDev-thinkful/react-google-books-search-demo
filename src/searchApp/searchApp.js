412be119afa8d8a5847bae2f41b6a966954930c4
grobelDev <grobeldev@gmail.com>
2019-11-11 08:05:26 -0800

    fixed some incompatibilities

--- src/searchApp/searchApp.js
+++ src/searchApp/searchApp.js
@@ -1,4 +1,4 @@
-import React, { Component } from './node_modules/react';
+import React, { Component } from 'react';
 
 // const fetchData = async () => {
 //   return fetch('https://www.googleapis.com/books/v1/volumes?q=henry')
@@ -31,86 +31,88 @@ class SearchApp extends Component {
       const json = await data.json()
       console.log(json);
 
-      results = json.items.map(item => {
-        turn {
-      volumeInfo: item.volumeInfo,
+      // let results = [];
+      // Get test first item.
+      let results = json.items.map(item => {
+        return {
+          volumeInfo: item.volumeInfo,
           saleInfo: item.saleInfo
         }
-  });
+      });
 
-  console.log(results);
-  
-  // let testItem = json.items[0];
-  // console.log(testItem);
-  // let testItemParse = {
-  //   volumeInfo: testItem.volumeInfo,
-  //   saleInfo: testItem.saleInfo
+      console.log(results);
+
+      // let testItem = json.items[0];
+      // console.log(testItem);
+      // let testItemParse = {
+      //   volumeInfo: testItem.volumeInfo,
+      //   saleInfo: testItem.saleInfo
       // }
-  // console.log(testItemParse);
-  
-  catch (error) {
+      // console.log(testItemParse);
+
+    } catch (error) {
       // do something with error
-  console.error(error);
+      console.error(error);
 
     }
 
-}
+  }
 
-// handleSubmit(e) {
-//   e.preventDefault();
-//   // alert('hi');
-//   // const bookmark = (({title, url, description, rating}) => ({title, url, description, rating}))(this.state);
-//   const url = 'https://www.googleapis.com/books/v1/volumes?q=henry';
-//   const options = {
-//     method: 'GET',
-//     // body: JSON.stringify(bookmark),
-//     // headers: {
-//     //   "Content-Type": "application/json",
-//     //   "Authorization": "Bearer $2a$10$ZhdeJefcb.5sx/DCmO/n8u5sJLcARAdbHw9tfm1mevGRq3s1.5DpW"
+  // handleSubmit(e) {
+  //   e.preventDefault();
+  //   // alert('hi');
+  //   // const bookmark = (({title, url, description, rating}) => ({title, url, description, rating}))(this.state);
+  //   const url = 'https://www.googleapis.com/books/v1/volumes?q=henry';
+  //   const options = {
+  //     method: 'GET',
+  //     // body: JSON.stringify(bookmark),
+  //     // headers: {
+  //     //   "Content-Type": "application/json",
+  //     //   "Authorization": "Bearer $2a$10$ZhdeJefcb.5sx/DCmO/n8u5sJLcARAdbHw9tfm1mevGRq3s1.5DpW"
   //     // }
-//   };
+  //   };
 
-//   fetch(url, options)
-//     .then(res => {
-//       if (!res.ok) {
-//         throw new Error('Something went wrong, please try again later');
-//       }
-//       return res.json();
-//     })
-//     .then(data => {
-//       // this.setState({
-//       //   title: "",
-//       //   url: "",
-//       //   description: "",
-//       //   rating: 1
-//       // });
-//       console.log(data);
-//       // this.props.handleAdd(bookmark);
-//       // let volumeInfo = data.items[0].volumeInfo;
-//       // let saleInfo = data.items[0].saleInfo;
-//       let results = this.processResults(data);
-//       this.props.handleSearch(results);
-//     })
-//     .catch(err => {
-//       this.setState({
-//         error: err.message
-//       });
+  //   fetch(url, options)
+  //     .then(res => {
+  //       if (!res.ok) {
+  //         throw new Error('Something went wrong, please try again later');
+  //       }
+  //       return res.json();
+  //     })
+  //     .then(data => {
+  //       // this.setState({
+  //       //   title: "",
+  //       //   url: "",
+  //       //   description: "",
+  //       //   rating: 1
+  //       // });
+  //       console.log(data);
+  //       // this.props.handleAdd(bookmark);
+  //       // let volumeInfo = data.items[0].volumeInfo;
+  //       // let saleInfo = data.items[0].saleInfo;
+  //       let results = this.processResults(data);
+  //       this.props.handleSearch(results);
+  //     })
+  //     .catch(err => {
+  //       this.setState({
+  //         error: err.message
+  //       });
   //     });
   // }
 
-  
-    er() {
-      rn (
-      orm className="addbookmark__form" onSubmit={e => this.handleSubmit(e)}>
-        abel htmlFor="search">Search:</label>
-        nput
-        type="text"
-        name="search"
-        id="search"
-        placeholder="Search"
-        onChange={e => this.searchChanged(e.target.value)}
-      />
-    </form>
+
+  render() {
+    return (
+      <form className="addbookmark__form" onSubmit={e => this.handleSubmit(e)}>
+        <label htmlFor="search">Search:</label>
+        <input
+          type="text"
+          name="search"
+          id="search"
+          placeholder="Search"
+          onChange={e => this.searchChanged(e.target.value)}
+        />
+      </form>
     );
   }
 }
